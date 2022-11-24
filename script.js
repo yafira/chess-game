4e2004a80c15e6bc5887def99f54a09468feb01a
Yafira <yafira.martinez@lc.cuny.edu>
2022-11-23 22:10:56 -0500

    added all chess pieces to board :)

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ var center = document.createElement('center')
 let chessBoard = document.createElement('board')
 // chessbaord array filling 64 spaces
 const boardArr = [
-	['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'],
+	[
+		'light-rook',
+		'light-knight',
+		'light-bishop',
+		'light-queen',
+		'light-king',
+		'light-bishop2',
+		'light-knight2',
+		'light-rook2',
+	],
 	[
 		'light-pawn',
 		'light-pawn',
@@ -28,7 +37,16 @@ const boardArr = [
 		'dark-pawn',
 		'dark-pawn',
 	],
-	['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'],
+	[
+		'dark-rook',
+		'dark-knight',
+		'dark-bishop',
+		'dark-queen',
+		'dark-king',
+		'dark-bishop2',
+		'dark-knight2',
+		'dark-rook2',
+	],
 ]
 
 for (let y = 0; y < 8; y++) {
@@ -41,16 +59,81 @@ for (let y = 0; y < 8; y++) {
 		// creating cell
 		let cell = document.createElement('td')
 		cell.classList.add('cell')
-
+		// light chess pieces
 		if (boardArr[y][x] === 'light-pawn') {
 			cell.classList.add('light-pawn')
 			// row.appendChild(cell)
 		}
-
+		if (boardArr[y][x] === 'light-rook') {
+			cell.classList.add('light-rook')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'light-rook2') {
+			cell.classList.add('light-rook2')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'light-knight') {
+			cell.classList.add('light-knight')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'light-knight2') {
+			cell.classList.add('light-knight2')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'light-bishop') {
+			cell.classList.add('light-bishop')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'light-bishop2') {
+			cell.classList.add('light-bishop2')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'light-queen') {
+			cell.classList.add('light-queen')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'light-king') {
+			cell.classList.add('light-king')
+			// row.appendChild(cell)
+		}
+		// dark chess pieces
 		if (boardArr[y][x] === 'dark-pawn') {
 			cell.classList.add('dark-pawn')
 			// row.appendChild(cell)
 		}
+		if (boardArr[y][x] === 'dark-rook') {
+			cell.classList.add('dark-rook')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'dark-rook2') {
+			cell.classList.add('dark-rook2')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'dark-knight') {
+			cell.classList.add('dark-knight')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'dark-knight2') {
+			cell.classList.add('dark-knight2')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'dark-bishop') {
+			cell.classList.add('dark-bishop')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'dark-bishop2') {
+			cell.classList.add('dark-bishop2')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'dark-queen') {
+			cell.classList.add('dark-queen')
+			// row.appendChild(cell)
+		}
+		if (boardArr[y][x] === 'dark-king') {
+			cell.classList.add('dark-king')
+			// row.appendChild(cell)
+		}
+
 		// white cells
 		if ((y + x) % 2 == 0) {
 			// class attribute for all white cells
